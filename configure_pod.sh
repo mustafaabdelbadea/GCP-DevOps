08fb008c4f90907094ef52e9b3ee6727051dfa03
Mustafa <mustafa.abdelbadea@gmail.com>
2023-05-28 04:23:21 +0300

    Add docker configuration

--- configure_pod.sh
+++ configure_pod.sh
@@ -7,3 +7,15 @@ curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/b
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+
+########## Docker ############
+
+    apt-get update && \
+    apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release && \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+    apt-get update && \
+    apt-get install -y docker-ce-cli && \
+    rm -rf /var/lib/apt/lists/*
